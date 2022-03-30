3a5ce6a2f217fca8e876cdfc5d6095dce6a6b980
Daniel Cabrera <danielfcabrerac@gmail.com>
2022-03-30 12:20:08 -0500

    adding contact-list

--- contact-list.js
+++ contact-list.js
@@ -1,7 +1,34 @@
-const edad = prompt("introduzca su edad");
+let contact = prompt("Introduce un contacto");
 
-if (edad >= 18) {
-  alert("si puede tomar");
-} else if (edad < 18) {
-  alert("no puede tomar");
+let contactName = [
+  "David Cabrera",
+  "Santiago Martinez",
+  "Jose Gonzalez",
+  "Colin Campbell",
+];
+
+addContact(contactName);
+
+function addContact(contactName) {
+  let newContact = contactName.push(contact);
 }
+alert(contactName);
+
+let contactToDelete;
+function borrarContacto(contactToDelete) {
+  for (i = 0; i <= contactName.length - 1; i++) {
+    if (contactName[i] == contactToDelete) {
+      contactName.splice(i, 1);
+    }
+  }
+  return contactName;
+}
+
+alert("Elimina Contacto");
+alert(borrarContacto(prompt(contactToDelete)));
+
+function printList() {
+  const newPrint = console.log(contactName);
+}
+printList(contactName);
+alert(contactName);
